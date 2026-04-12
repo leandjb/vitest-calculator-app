d7b631c57967c54eac43d30ec23aa72d61cb0911
Leandro Barros <leandjb@gmail.com>
2026-04-11 19:11:15 -0500

    added new format

--- README.md
+++ README.md
@@ -16,7 +16,9 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 
 ## Running unit tests
 
-Run `ng test` to execute the unit tests via Vitest
+Run `ng test` to execute the unit tests via Vites
+
+t
 
 ## Running end-to-end tests
 
--- src/app/calculator/services/calculator.service.spec.ts
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { CalculatorService } from './calculator.service';
-import { describe, it, test, expect, vi} from 'vitest';
+import { describe, it, test, expect, vi } from 'vitest';
 
 describe('CalculatorServices', () => {
 
 	let service: CalculatorService;
 
-	beforeEach( ()=> {
+	beforeEach(() => {
 		TestBed.configureTestingModule({});
 		service = TestBed.inject(CalculatorService);
 
@@ -18,7 +18,7 @@ describe('CalculatorServices', () => {
 		expect(service).toBeTruthy();
 	});
 
-	it('should be created with default values', ()=> {
+	it('should be created with default values', () => {
 
 		//console.log(service);
 		expect(service.resultText()).toBe("0")
@@ -26,22 +26,22 @@ describe('CalculatorServices', () => {
 		expect(service.lastOperator()).toBe("+")
 	})
 
-	it('should set resultText, subResultText to "0" when C is pressed', ()=> {
+	it('should set resultText, subResultText to "0" when C is pressed', () => {
 
 		service.resultText.set('654')
 		service.resultText.set('987')
 
 		//console.log({ resultTextValue: service.resultText() })
-		
+
 		service.constructNumber('C');
-		
+
 		//console.log({ resultTextValue: service.resultText() })
 
 		expect(service.resultText()).toBe('0');
 		expect(service.subResultText()).toBe('0');
 	})
 
-	it('should update resultText with 4 number inputs', ()=> {
+	it('should update resultText with 4 number inputs', () => {
 
 		service.constructNumber('1')
 		service.constructNumber('9')
@@ -51,7 +51,7 @@ describe('CalculatorServices', () => {
 
 	})
 
-	it('should handle operators correctly', ()=> {
+	it('should handle operators correctly', () => {
 		const operators = ['+', '-', '*', '/']
 
 		operators.forEach((operator) => {
@@ -60,10 +60,10 @@ describe('CalculatorServices', () => {
 			expect(service.lastOperator()).toBe(operator);
 			expect(service.resultText()).toBe('0');
 		});
-			
+
 	})
 
-	it('should calculate result correctly for addition', ()=> {
+	it('should calculate result correctly for addition', () => {
 		service.constructNumber('9')
 		service.constructNumber('+')
 		service.constructNumber('8')
@@ -72,7 +72,7 @@ describe('CalculatorServices', () => {
 		expect(service.resultText()).toBe('17')
 	})
 
-	it('should calculate result correctly for substraction', ()=> {
+	it('should calculate result correctly for substraction', () => {
 		service.constructNumber('7')
 		service.constructNumber('-')
 		service.constructNumber('6')
@@ -80,8 +80,8 @@ describe('CalculatorServices', () => {
 
 		expect(service.resultText()).toBe('1')
 	})
-	
-	it('should calculate result correctly for multiplication', ()=> {
+
+	it('should calculate result correctly for multiplication', () => {
 		service.constructNumber('5')
 		service.constructNumber('*')
 		service.constructNumber('4')
@@ -89,8 +89,8 @@ describe('CalculatorServices', () => {
 
 		expect(service.resultText()).toBe('20')
 	})
-	
-	it('should calculate result correctly for division', ()=> {
+
+	it('should calculate result correctly for division', () => {
 		service.constructNumber('9')
 		service.constructNumber('/')
 		service.constructNumber('3')
@@ -99,7 +99,7 @@ describe('CalculatorServices', () => {
 		expect(service.resultText()).toBe('3')
 	})
 
-	it('should handle decimal point', ()=>{
+	it('should handle decimal point', () => {
 		service.constructNumber('8');
 		service.constructNumber('.');
 		service.constructNumber('1');
@@ -110,9 +110,9 @@ describe('CalculatorServices', () => {
 		service.constructNumber('=');
 
 		expect(service.resultText()).toBe('4.05');
-	})	
+	})
 
-	it('should handle with double decimal point', ()=>{
+	it('should handle with double decimal point', () => {
 		service.constructNumber('9');
 		service.constructNumber('7');
 		service.constructNumber('2');
@@ -124,23 +124,23 @@ describe('CalculatorServices', () => {
 		service.constructNumber('1');
 		service.constructNumber('.');
 		service.constructNumber('.');
-	
+
 		expect(service.resultText()).toBe('972.5301')
 	})
 
 
-	it('should handle sign change +/-', ()=>{
-		service.constructNumber('1')	
-		service.constructNumber('9')	
-		service.constructNumber('0')	
-		service.constructNumber('8')	
-		service.constructNumber('+/-')	
+	it('should handle sign change +/-', () => {
+		service.constructNumber('1')
+		service.constructNumber('9')
+		service.constructNumber('0')
+		service.constructNumber('8')
+		service.constructNumber('+/-')
 		expect.soft(service.resultText()).toBe('-1908')
-		service.constructNumber('+/-')	
+		service.constructNumber('+/-')
 		expect(service.resultText()).toBe('1908')
 	})
 
-	it('should handle backspace', ()=>{
+	it('should handle backspace', () => {
 		service.constructNumber('6')
 		service.constructNumber('4')
 		expect(service.resultText()).toBe('64')
@@ -152,7 +152,7 @@ describe('CalculatorServices', () => {
 		expect(service.resultText()).toBe('0')
 	})
 
-	it('should handle backspace with negative numbers', ()=>{
+	it('should handle backspace with negative numbers', () => {
 		service.resultText.set('-15')
 		service.constructNumber('Backspace')
 
@@ -161,11 +161,11 @@ describe('CalculatorServices', () => {
 		expect(service.resultText()).toBe('0')
 	})
 
-	it('should handle max length to be 10 characters', ()=>{
+	it('should handle max length to be 10 characters', () => {
 		const consoleSpy = vi
 		const maxLength = 10
 
-		for(let i=0; i < maxLength; i++){
+		for (let i = 0; i < maxLength; i++) {
 			service.constructNumber('3')
 		}
 
@@ -173,14 +173,14 @@ describe('CalculatorServices', () => {
 		expect(service.resultText()).toBe('3333333333')
 	})
 
-	it('should decimal number start with zero', ()=> {
+	it('should decimal number start with zero', () => {
 		service.constructNumber('.')
 		service.constructNumber('.')
 
 		expect(service.resultText()).toBe('0.')
 	})
 
-	it('should handle invalid input with SPY', ()=>{
+	it('should handle invalid input with SPY', () => {
 		const consoleSpy = vi.spyOn(console, 'log')
 		const wrongInput: string = 'ABC'
 
@@ -196,14 +196,14 @@ describe('CalculatorServices', () => {
 
 	})
 
-	it('should have max 10 characters with SPY', ()=>{
+	it('should have max 10 characters with SPY', () => {
 		const consoleSpy = vi.spyOn(console, 'log')
 
 		//delete logs message on console
-		consoleSpy.mockImplementation( ()=>{});
+		consoleSpy.mockImplementation(() => { });
 
 
-		for (let  i=0; i<20; i++)  {
+		for (let i = 0; i < 20; i++) {
 			service.constructNumber('1');
 		}
 
